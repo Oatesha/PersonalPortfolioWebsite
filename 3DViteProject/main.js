de290a94c46059420a8430fa9338a10f6557970b
Oatesha <73292759+Oatesha@users.noreply.github.com>
2024-02-20 13:51:01 +0800

    working spheres in shape of text

---
+++ 3DViteProject/main.js
@@ -0,0 +1,186 @@
+import './style.css'
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+
+// Set up scene
+const scene = new THREE.Scene();
+
+// Set up camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 100;
+
+// Set up renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#Background'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Create lights
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const textureFontSize = 100;
+const fontScaleFactor = .3;
+
+// Settings
+const fontName = 'Verdana';
+
+// String to show
+let string = 'Some text' + '\n' + 'to sample' + '\n' + 'with Canvas';
+let dummy, particleGeometry, particleMaterial, instancedMesh
+let textureCoordinates;
+let mouse;
+
+mouse = new THREE.Vector2(-200, 200);
+
+
+let stringBox = {
+  wTexture: 0,
+  wScene: 0,
+  hTexture: 0,
+  hScene: 0
+};
+// Create canvas to sample the text
+const textCanvas = document.createElement('canvas');
+const textCtx = textCanvas.getContext('2d');
+document.body.appendChild(textCanvas);
+// Instanced geometry and material
+particleGeometry = new THREE.SphereGeometry(.1);
+particleMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+
+dummy = new THREE.Object3D();
+
+
+
+// ---------------------------------------------------------------
+refreshText();
+initEvents();
+
+
+function initEvents() {
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  // document.addEventListener( 'mousemove', onMouseMove.bind( this ));
+
+  // onMouseMove() { 
+
+  //   this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  //   this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  // }
+
+}
+
+function refreshText() {
+  sampleCoordinates();
+  textureCoordinates = textureCoordinates.map((c) => {
+    return { x: c.x * fontScaleFactor, y: c.y * fontScaleFactor };
+  });
+  const maxX = textureCoordinates.map((v) => v.x).sort((a, b) => b - a)[0];
+  const maxY = textureCoordinates.map((v) => v.y).sort((a, b) => b - a)[0];
+  stringBox.wScene = maxX;
+  stringBox.hScene = maxY;
+
+  createInstancedMesh();
+  updateParticlesMatrices();
+}
+// ---------------------------------------------------------------
+
+function sampleCoordinates() {
+  // Parse text
+  const lines = string.split(`\n`);
+  const linesMaxLength = [...lines].sort((a, b) => b.length - a.length)[0]
+    .length;
+  stringBox.wTexture = textureFontSize * 0.7 * linesMaxLength;
+  stringBox.hTexture = lines.length * textureFontSize;
+
+  // Draw text
+  const linesNumber = lines.length;
+  textCanvas.width = stringBox.wTexture;
+  textCanvas.height = stringBox.hTexture;
+  textCtx.font = "100 " + textureFontSize + "px " + fontName;
+  textCtx.fillStyle = "#2a9d8f";
+  textCtx.clearRect(0, 0, textCanvas.width, textCanvas.height);
+  for (let i = 0; i < linesNumber; i++) {
+    textCtx.fillText(
+      lines[i],
+      0,
+      ((i + 0.8) * stringBox.hTexture) / linesNumber
+    );
+  }
+
+  // Sample coordinates
+  textureCoordinates = [];
+  if (stringBox.wTexture > 0) {
+    const imageData = textCtx.getImageData(
+      0,
+      0,
+      textCanvas.width,
+      textCanvas.height
+    );
+    for (let i = 0; i < textCanvas.height; i++) {
+      for (let j = 0; j < textCanvas.width; j++) {
+        if (imageData.data[(j + i * textCanvas.width) * 4] > 0) {
+          textureCoordinates.push({
+            x: j,
+            y: i
+          });
+        }
+      }
+    }
+  }
+}
+
+// ---------------------------------------------------------------
+// Handle points
+
+
+function createInstancedMesh() {
+  instancedMesh = new THREE.InstancedMesh(particleGeometry, particleMaterial, textureCoordinates.length);
+  scene.add(instancedMesh);
+
+  // centralize it in the same way as before
+  instancedMesh.position.x = -.5 * stringBox.wScene;
+  instancedMesh.position.y = -.5 * stringBox.hScene;
+}
+
+function updateParticlesMatrices() {
+  let idx = 0;
+  textureCoordinates.forEach(p => {
+
+      // we apply samples coordinates like before + some random rotation
+      dummy.rotation.set(2 * Math.random(), 2 * Math.random(), 2 * Math.random());
+      dummy.position.set(p.x, stringBox.hScene - p.y, Math.random());
+
+      dummy.updateMatrix();
+      instancedMesh.setMatrixAt(idx, dummy.matrix);
+
+      idx ++;
+  })
+  instancedMesh.instanceMatrix.needsUpdate = true;
+}
+
+// Animation function
+function animate() {
+    requestAnimationFrame(animate);
+
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+
+// Start animation
+animate();
