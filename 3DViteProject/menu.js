85b3dc464782d3f97bbc8e6110756648b3ad6a29
Oatesha <73292759+Oatesha@users.noreply.github.com>
2024-03-26 09:37:26 +0800

    project section layout

--- 3DViteProject/menu.js
+++ 3DViteProject/menu.js
@@ -2,22 +2,37 @@ import { gsap } from "gsap";
 
 const hamburgerMenu = document.querySelector(".HamburgerToggle");
 const menu = document.querySelector(".MenuElements");
-console.log(menu);
+const blurBackground = document.querySelector(".menuBlur");
+const buttonSVGOne = document.querySelector("#buttonSVGOne");
+const buttonSVGTwo = document.querySelector("#buttonSVGTwo");
+console.log(buttonSVGOne);
 
 hamburgerMenu.addEventListener("click", toggleMenu);
 hamburgerMenu.addEventListener("mouseenter", hoverHamburger);
 hamburgerMenu.addEventListener("mouseleave", exitHoverHamburger);
 
+// buttonSVGOne.addEventListener("click", toggleMenu);
+// buttonSVGOne.addEventListener("mouseenter", hoverHamburger);
+// buttonSVGOne.addEventListener("mouseleave", exitHoverHamburger);
+
+// buttonSVGTwo.addEventListener("click", toggleMenu);
+// buttonSVGTwo.addEventListener("mouseenter", hoverHamburger);
+// buttonSVGTwo.addEventListener("mouseleave", exitHoverHamburger);
+
 let menuVisible = false;
-let targetAlpha;
+let targetAlpha, targetZ;
 
 function toggleMenu() {
     menuVisible ? targetAlpha = 0.0 : targetAlpha = 1.0; 
+    menuVisible ? targetZ = 0.0 : targetZ = 5.0; 
     menuVisible = !menuVisible;
 
     gsap.to(menu, {
         autoAlpha: targetAlpha,
         duration: 0.5,
+    })
+    gsap.to(blurBackground, {
+        autoAlpha: targetAlpha,
     })
 }
 
